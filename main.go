9d6bf47928d27445ad839e87e89bf546f24d8278
Louis <vanaardtlj03@gmail.com>
2025-07-23 08:54:07 +0200

    added cors exception

--- main.go
+++ main.go
@@ -129,7 +129,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		if origin == "http://localhost:8081" || origin == "http://127.0.0.1:8081" {
+		if origin == "http://localhost:8081" || origin == "http://127.0.0.1:8081" || origin == "http://127.0.0.1:5173" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
